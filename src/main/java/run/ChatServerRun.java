121fa36716c7b4beeabe0311a2595278e64e81cd
iseungho <misstal@naver.com>
2022-08-21 17:29:38 +0900

    대기 while 문 추가

--- src/main/java/run/ChatServerRun.java
+++ src/main/java/run/ChatServerRun.java
@@ -1,12 +1,17 @@
 package run;
 
+import java.util.Date;
+
 public class ChatServerRun {
 
     /**
      * Server Main
      * @param args
      */
-    public void main(String[]   args) {
-
+    public static void main(String[]   args) {
+        //
+        while (true) {
+            System.out.println(new Date());
+        }
     }
 }
